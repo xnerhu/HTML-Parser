11126f8d5b1dbb74e4b83b1d0b60f10c682ddee4
unknown <xnerhu@gmail.com>
2018-09-21 16:26:25 +0200

    Add tokenizer

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace HTMLParser {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string indexPath = "./assets/index.html";
+            string sourceCode = File.ReadAllText(indexPath, Encoding.UTF8);
+
+            List<string> tokens = Tokenizer.Tokenize(sourceCode);
+
+            foreach (string token in tokens) {
+                Console.WriteLine(token);
+            }
+
             Console.ReadLine();
         }
     }
---
+++ src/Tokenizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HTMLParser {
+    public static class Tokenizer {
+        public static List<string> Tokenize(string source) {
+            List<string> list = new List<string>();
+
+            string text = "";
+            bool capturingTag = false;
+
+            for (int i = 0; i < source.Length; i++) {
+                 if (source[i] == '<') {
+                    if (text.Length > 0) {
+                        list.Add(text);
+                        text = "";
+                    }
+
+                    capturingTag = true;
+                } else if (source[i] == '>' && capturingTag) {
+                    list.Add(text + '>');
+                    text = "";
+                    capturingTag = false;
+                    continue;
+                }
+
+                text += source[i];
+            }
+
+            return list;
+        }
+    }
+}
