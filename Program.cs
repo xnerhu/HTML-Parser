9bac771850c3430871b9f864f56087dac329e426
unknown <xnerhu@gmail.com>
2018-09-25 20:17:56 +0200

    Add SourceHandler

--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace HTMLParser {
     class Program {
+        static string AssetsPath = AppDomain.CurrentDomain.BaseDirectory + "assets/";
+        static string IndexPath = AssetsPath + "index.html";
+
         static void Main(string[] args) {
             Console.OutputEncoding = Encoding.UTF8;
 
-            string indexPath = "./assets/index.html";
-            string sourceCode = File.ReadAllText(indexPath, Encoding.UTF8);
+            string path = IndexPath;
+            string sourceCode = SourceHandler.Request(new Uri(path));
 
             HTMLDocument document = new HTMLDocument(sourceCode);
 
---
+++ src/utils/SourceHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HTMLParser {
+    public static class SourceHandler {
+        public const string DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" +
+            " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
+
+        public static string Request(Uri uri, string userAgent = DefaultUserAgent) {
+            if (uri.IsFile) {
+                try {
+                    return File.ReadAllText(uri.AbsolutePath, Encoding.UTF8);
+                } catch (IOException e) {
+                    Console.WriteLine(e.Message);
+                }
+            } else {
+                try {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                    request.UserAgent = userAgent;
+
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream)) {
+                        return reader.ReadToEnd();
+                    }
+                } catch (WebException e) {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            return null;
+        }
+
+    }
+}
